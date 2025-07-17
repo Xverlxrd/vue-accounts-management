8a274409b2eacace34e6ece0940922bc07abb18b
XverLxrd <tur-2002@mail.ru>
2025-07-17 20:27:07 +0700

    feat(accounts): implement AccountsList and AccountsLabelColumn components

---
+++ src/components/AccountLabelColumn/AccountLabelColumn.vue
@@ -0,0 +1,24 @@
+<template>
+  <div :class="['account-list__column', `account-list__column--${columnKey}`]">
+    <a-tooltip placement="top" :title="tooltip">
+      <span class="account-list__column-title">{{ title }}</span>
+    </a-tooltip>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  columnKey: {
+    type: String,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  tooltip: {
+    type: String,
+    required: true
+  }
+})
+</script>
---
+++ src/constants/accountLabels.const.ts
@@ -0,0 +1,22 @@
+export const ACCOUNT_LABELS = [
+    {
+        key: 'label',
+        title: 'Метка',
+        tooltip: 'Метка для идентификации учетной записи'
+    },
+    {
+        key: 'type',
+        title: 'Тип записи',
+        tooltip: 'Тип учетной записи (Локальная/LDAP)'
+    },
+    {
+        key: 'login',
+        title: 'Логин',
+        tooltip: 'Имя пользователя для входа'
+    },
+    {
+        key: 'password',
+        title: 'Пароль',
+        tooltip: 'Пароль для авторизации'
+    }
+];
