d6d3bf508dcbf6b10fa1b9a307f09ed05eac85ce
XverLxrd <tur-2002@mail.ru>
2025-07-17 19:34:55 +0700

    styles: add CSS reset

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
+import Antd from 'ant-design-vue'
 import './styles/main.css'
 
 const pinia = createPinia()
 const app = createApp(App)
 
+app.use(Antd)
 app.use(pinia)
 app.mount('#app')
